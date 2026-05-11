a400d36db745d0f32755e5d73e8f6e4316f28d61
WYPanda <1825679767@qq.com>
2026-05-11 09:26:28 +0800

    1

--- src/shared/revision.h
+++
@@ -1,5 +0,0 @@
-#ifndef __REVISION_DATA_H__
-#define __REVISION_DATA_H__
- #define REVISION_HASH                      "46f148ac560b1ce9dfe1"
- #define REVISION_DATE                      "2026-05-07 15:01:26 +0800"
-#endif // __REVISION_DATA_H__
